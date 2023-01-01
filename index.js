840d63f5e6aa9f8115e57e99eb52e94623ddaf0e
praveenniroula1 <101380394+praveenniroula1@users.noreply.github.com>
2023-01-02 10:41:31 +1100

    debugging

--- index.js
+++ index.js
@@ -10,23 +10,27 @@ let hrsData = [""];
 let toDoList = [];
 let notToDoList = [];
 
-const handleOnSubmit = () => {
-  taskName.addEventListener("change", () => {
-    taskData = taskName.value;
-  });
-  taskHrs.addEventListener("change", () => {
-    hrsData = taskHrs.value;
-  });
-  submit.addEventListener("click", () => {
-    const data = {
-      taskData,
-      hrsData,
-    };
-    toDoList.push(data);
+function handleOnDelete(i) {
+  const filteredArray = toDoList.filter((item, index) => index !== i);
+  toDoList = filteredArray;
+  console.log(toDoList);
+}
+taskName.addEventListener("change", () => {
+  taskData = taskName.value;
+});
+taskHrs.addEventListener("change", () => {
+  hrsData = taskHrs.value;
+});
+submit.addEventListener("click", () => {
+  const data = {
+    taskData,
+    hrsData,
+  };
+  toDoList.push(data);
 
-    let str = "";
-    toDoList.map((item, i) => {
-      str += `<tr>
+  let str = "";
+  toDoList.forEach((item, i) => {
+    str += `<tr>
      <th id="i" scope="row">${i + 1}</th>
      <td id="list">${item.taskData + " " + item.hrsData}</td>
      <td class="font">
@@ -38,30 +42,42 @@ const handleOnSubmit = () => {
        </button>
      </td>
    </tr>`;
-      document.getElementById("tbody").innerHTML = str;
-    });
+    document.getElementById("tbody").innerHTML = str;
   });
-};
-handleOnSubmit();
+});
+
+// handleOnSubmit();
 
 // handling the delete button
-const handleOnDelete = (i) => {
-  const toDelete = toDoList.filter((item, index) => index !== i);
-  toDoList = toDelete;
-};
-handleOnDelete();
 
-// handling the switch buttons
-const handleOnSwitch = (i) => {
-  const itemToSwitch = toDoList.splice(i, 1);
-  notToDoList.push(itemToSwitch[0]);
-  toDoList();
-};
-handleOnSwitch();
+//   toDoList.map((item, i) => {
+//     str += `<tr>
+//    <th id="i" scope="row">${i + 1}</th>
+//    <td id="list">${item.taskData + " " + item.hrsData}</td>
+//    <td class="font">
+//      <button class="button2 bg-success">
+//        <i class="fa-solid fa-hand-point-right"></i>
+//      </button>
+//      <button onclick="handleOnDelete(${i})"  class="button2 bg-danger">
+//        <i class="fa-sharp fa-solid fa-trash"></i>
+//      </button>
+//    </td>
+//  </tr>`;
+//     document.getElementById("tbody").innerHTML = str;
+//   });
+// };
 
-const handleOnSwitchBack = (i) => {
-  const itemToSwitchBack = notToDoList.splice(i, 1);
-  notToDoList.push(itemToSwitchBack[0]);
-  toDoList();
-};
-handleOnSwitchBack();
+// // handling the switch buttons
+// const handleOnSwitch = (i) => {
+//   const itemToSwitch = toDoList.splice(i, 1);
+//   notToDoList.push(itemToSwitch[0]);
+//   toDoList();
+// };
+// handleOnSwitch();
+
+// const handleOnSwitchBack = (i) => {
+//   const itemToSwitchBack = notToDoList.splice(i, 1);
+//   notToDoList.push(itemToSwitchBack[0]);
+//   toDoList();
+// };
+// handleOnSwitchBack();
